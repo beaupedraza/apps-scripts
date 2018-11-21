af0b22d328742318824a8658a9016499e936c6b8
Beau Pedraza <beaupedraza@gmail.com>
2018-11-20 19:12:00 -0800

    uploaded base engine for v2

---
+++ googlesuggest.js
@@ -0,0 +1,26 @@
+/*
+
+----------------------------------------------
+Status: Work in progress
+
+
+Author: Beau Pedraza
+
+Description: Simple standalone function that pulls in Google Autosuggest terms
+Description: for a given seed keyword. 
+
+Lessons Learned #1: If you want the script to work, see client=
+
+----------------------------------
+
+
+*/
+
+function getGoogleSuggest(keyword) {
+
+  var googleSuggestUrl = 'http://suggestqueries.google.com/complete/search?client=firefox&hl=en&q=' + keyword;
+  var response = UrlFetchApp.fetch(googleSuggestUrl);
+  var keywords = JSON.parse(response.getContentText());
+
+  return keywords;
+}
